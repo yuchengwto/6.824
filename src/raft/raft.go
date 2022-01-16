b12f4c54ac5af200004e3f784a236c752fc7641b
cheng <yuchengscu@gmail.com>
2022-01-16 16:14:01 +0800

    finish lab2A

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -18,14 +18,16 @@ package raft
 //
 
 import (
-//	"bytes"
+	//	"bytes"
+
+	"math/rand"
 	"sync"
 	"sync/atomic"
+	"time"
 
-//	"6.824/labgob"
+	//	"6.824/labgob"
 	"6.824/labrpc"
 )
-
 
 //
 // as each Raft peer becomes aware that successive log entries are
@@ -64,6 +66,30 @@ type Raft struct {
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
 
+	// leader election.
+	// persistent state on all servers.
+	currentTerm int        // 当前任期
+	votedFor    int        // 投票的候选人id，-1表示未投票
+	log         []LogEntry // 初始索引为1
+	// volatile state on all servers.
+	commitIndex int // 该peer已知的最后commit的log entry的索引
+	lastApplied int // 应用到该peer状态机的log entry的最新索引
+	// volatile state on leaders.
+	// 每次选举后重新初始化
+	nextIndex  []int // 每个follower的应该接收的下一条log entry的索引，初始化为leader last log index + 1
+	matchIndex []int // 每个follower已知的已复制的log entry的最新索引，齿梳化为0
+
+	// election timeout相关
+	lastFlushTime   int64
+	electionTimeOut int64
+	// 当前状态，0follower，1candidate，2leader
+	state int
+}
+
+// log entry struct.
+type LogEntry struct {
+	Command string
+	Term    int
 }
 
 // return currentTerm and whether this server
@@ -73,6 +99,10 @@ func (rf *Raft) GetState() (int, bool) {
 	var term int
 	var isleader bool
 	// Your code here (2A).
+	rf.mu.Lock()
+	term = rf.currentTerm
+	isleader = (rf.state == 2)
+	rf.mu.Unlock()
 	return term, isleader
 }
 
@@ -91,7 +121,6 @@ func (rf *Raft) persist() {
 	// data := w.Bytes()
 	// rf.persister.SaveRaftState(data)
 }
-
 
 //
 // restore previously persisted state.
@@ -115,7 +144,6 @@ func (rf *Raft) readPersist(data []byte) {
 	// }
 }
 
-
 //
 // A service wants to switch to snapshot.  Only do so if Raft hasn't
 // have more recent info since it communicate the snapshot on applyCh.
@@ -136,13 +164,16 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 
 }
 
-
 //
 // example RequestVote RPC arguments structure.
 // field names must start with capital letters!
 //
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
+	Term         int // candidate's term
+	CandidateId  int
+	LastLogIndex int // index of candidate's last log entry
+	LastLogTerm  int // term of candidate's last log entry
 }
 
 //
@@ -151,6 +182,8 @@ type RequestVoteArgs struct {
 //
 type RequestVoteReply struct {
 	// Your data here (2A).
+	Term        int  // currentTerm, for candidate to update itself
+	VoteGranted bool // true means candidate received vote
 }
 
 //
@@ -158,6 +191,174 @@ type RequestVoteReply struct {
 //
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
+
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	// 设置回复的默认值
+	reply.Term = rf.currentTerm
+	reply.VoteGranted = false
+
+	if rf.currentTerm < args.Term {
+		// 如果对方的term更优，将peer的term更新到对方的term，同时转换状态为follower，重置其投票状态为-1
+		rf.currentTerm = args.Term
+		rf.setFollower()
+	}
+
+	if rf.currentTerm > args.Term {
+		// peer的term更优，投反对票，提前退出
+		reply.VoteGranted = false
+		return
+	}
+
+	if (rf.votedFor == -1) || (rf.votedFor == args.CandidateId) {
+		// peer尚未投票或投票对象为对方
+		// candidate或leader状态，peer的投票状态应该都为自己，不会进入该判断
+		if (args.LastLogTerm > rf.log[len(rf.log)-1].Term) ||
+			((args.LastLogTerm == rf.log[len(rf.log)-1].Term) && (args.LastLogIndex >= len(rf.log)-1)) {
+			// 对方的log任期更新，或对方的log任期相同且log index大于等于peer
+			rf.votedFor = args.CandidateId
+			reply.VoteGranted = true
+			rf.flushElectionTimer()
+		}
+	}
+}
+
+type AppendEntriesArgs struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      []LogEntry
+	LeaderCommit int
+}
+
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
+
+func (rf *Raft) setFollower() {
+	rf.state = 0
+	rf.votedFor = -1
+}
+
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	// 设置回复的默认值
+	reply.Term = rf.currentTerm
+	reply.Success = false
+
+	if rf.currentTerm <= args.Term {
+		// 如果对方term未过期，刷新选举timer
+		rf.flushElectionTimer()
+
+		if rf.currentTerm < args.Term {
+			// 如果对方的term更优，将peer的term更新到对方的term，同时转换状态为follower
+			rf.currentTerm = args.Term
+			rf.setFollower()
+		}
+	}
+}
+
+func (rf *Raft) heartBeats() {
+	// leader的心跳协程，只要peer还是leader，就会不断循环下去
+	var state int
+	for {
+		rf.mu.Lock()
+		state = rf.state
+		rf.mu.Unlock()
+		// 判断是否为leader，如果不是，退出循环，不再发送心跳请求
+		if state != 2 {
+			break
+		}
+
+		// 先把自己的选举timer刷新一下
+		rf.mu.Lock()
+		rf.flushElectionTimer()
+		rf.mu.Unlock()
+		for idx := range rf.peers {
+			// 按idx给其他服务器发送心跳
+			if idx == rf.me {
+				continue
+			}
+
+			// 给单个服务器发送心跳的协程
+			go rf.heartBeatsSingle(idx)
+		}
+
+		time.Sleep(100 * time.Millisecond) // 100ms一次，这样1秒就是10次内，符合规定
+	}
+}
+
+func (rf *Raft) heartBeatsSingle(server int) {
+	args := AppendEntriesArgs{}
+	reply := AppendEntriesReply{}
+	rf.mu.Lock()
+	args.Term = rf.currentTerm
+	args.LeaderId = rf.me
+	args.PrevLogIndex = 0
+	args.PrevLogTerm = 0
+	args.Entries = make([]LogEntry, 0) // 心跳请求，log为空数组
+	args.LeaderCommit = rf.commitIndex
+	rf.mu.Unlock()
+
+	// 发出心跳请求，无论是否成功，都继续，不需要重试
+	ok := rf.peers[server].Call("Raft.AppendEntries", &args, &reply)
+	if ok {
+		// 对返回的响应做判断
+		// 判断响应的term
+		rf.mu.Lock()
+		currentTerm := rf.currentTerm
+		rf.mu.Unlock()
+		if currentTerm < reply.Term {
+			rf.mu.Lock()
+			rf.currentTerm = reply.Term
+			rf.setFollower()
+			rf.mu.Unlock()
+		}
+	}
+}
+
+func (rf *Raft) getVoteSingle(server int, ch chan<- bool) {
+	args := RequestVoteArgs{}
+	reply := RequestVoteReply{}
+	rf.mu.Lock()
+	// 获取刷新时间和超时时间
+	// 这两个时间在该协程内应该是不变的
+	flushtime := rf.lastFlushTime
+	electtionTimeOut := rf.electionTimeOut
+	rf.mu.Unlock()
+	var currentTime int64
+	for {
+		currentTime = time.Now().UnixMilli()
+		if (currentTime - flushtime) > electtionTimeOut {
+			// 超时，退出投票协程
+			break
+		}
+
+		rf.mu.Lock()
+		// args参数赋值
+		args.Term = rf.currentTerm
+		args.CandidateId = rf.me
+		args.LastLogIndex = len(rf.log) - 1
+		args.LastLogTerm = rf.log[len(rf.log)-1].Term
+		rf.mu.Unlock()
+		ok := rf.peers[server].Call("Raft.RequestVote", &args, &reply)
+		if ok {
+			rf.mu.Lock()
+			// 判断响应的term
+			if rf.currentTerm < reply.Term {
+				rf.currentTerm = reply.Term
+				rf.setFollower()
+			}
+			rf.mu.Unlock()
+			ch <- reply.VoteGranted
+			break
+		}
+		// 如果不ok，就会一直重试直到超时
+	}
 }
 
 //
@@ -194,7 +395,6 @@ func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *Reques
 	return ok
 }
 
-
 //
 // the service using Raft (e.g. a k/v server) wants to start
 // agreement on the next command to be appended to Raft's log. if this
@@ -215,7 +415,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	isLeader := true
 
 	// Your code here (2B).
-
 
 	return index, term, isLeader
 }
@@ -241,6 +440,11 @@ func (rf *Raft) killed() bool {
 	return z == 1
 }
 
+func (rf *Raft) flushElectionTimer() {
+	rf.electionTimeOut = int64(rand.Intn(1000-500) + 500) // 选举超时设为500-1000ms之间的随机值
+	rf.lastFlushTime = time.Now().UnixMilli()             // 当前时间设为刷新时间
+}
+
 // The ticker go routine starts a new election if this peer hasn't received
 // heartsbeats recently.
 func (rf *Raft) ticker() {
@@ -250,6 +454,88 @@ func (rf *Raft) ticker() {
 		// be started and to randomize sleeping time using
 		// time.Sleep().
 
+		rf.mu.Lock()
+		currentTime := time.Now().UnixMilli()
+		flushTime := rf.lastFlushTime
+		electionTimeOut := rf.electionTimeOut
+		rf.mu.Unlock()
+		// 判断是否应该发起选举，如果最近刷新过选举timer，则跳过
+		if currentTime-flushTime > electionTimeOut {
+			// election timeout超时，开始选举
+			// fmt.Printf("%d start elec\n", rf.me)
+			rf.mu.Lock()
+			rf.state = 1                        // 状态转化为candidate
+			rf.currentTerm = rf.currentTerm + 1 // 任期加一
+			rf.votedFor = rf.me                 // 投自己
+			rf.flushElectionTimer()             // 刷新选举timer
+			rf.mu.Unlock()
+
+			// 向其他server发送投票请求
+			// fmt.Printf("%d req\n", rf.me)
+			chVotes := make(chan bool, len(rf.peers)) // buffer设置为server数
+			chVotes <- true                           // 先给自己来个yes
+			for idx := range rf.peers {
+				if rf.me == idx {
+					continue
+				}
+				// fmt.Printf("%d to %d req single\n", rf.me, idx)
+				go rf.getVoteSingle(idx, chVotes)
+			}
+
+			// 获取选举timer，在收集投票循环中不应该再更新，不然一直无法超时退出
+			rf.mu.Lock()
+			flushTime = rf.lastFlushTime
+			electionTimeOut = rf.electionTimeOut
+			rf.mu.Unlock()
+
+			// 收集投票
+			// fmt.Printf("%d collect\n", rf.me)
+			votes := 0
+			var state int
+			for {
+				currentTime = time.Now().UnixMilli()
+
+				// 判断超时或成为leader或有其他peer成为了leader
+				// 1. 当前peer已经转换为follower
+				// 当接收到其他peer的appendentries RPC时，表示其他peer已经成为leader，将转化为follower
+				// 当接收到其他peer的RPC中包含的term更新时，也会转化为follower
+				rf.mu.Lock()
+				state = rf.state
+				rf.mu.Unlock()
+				if state == 0 {
+					break
+				}
+				// 2. 收集到超过一半的投票，成为leader
+				if votes >= (len(rf.peers)/2 + 1) {
+					rf.mu.Lock()
+					// state设置为leader即2
+					rf.state = 2
+					rf.mu.Unlock()
+					// 开始干leader的活了
+					// go appendlog
+					go rf.heartBeats()
+					break
+				}
+				// 3. 超时，退出循环，理论上会再次进入该选举投票循环
+				if currentTime-flushTime > electionTimeOut {
+					break
+				}
+
+				select {
+				case v := <-chVotes:
+					// 通道中有投票信息，按照结果统计
+					if v {
+						// 如果投票结果v为true，加1分
+						votes += 1
+					}
+				default:
+					// 通道中没有投票信息，10ms内等待随机后重新尝试获取
+					// fmt.Printf("%d %d %d %d\n", rf.me, rf.currentTerm, currentTime, votes)
+					time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+				}
+			}
+		}
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond) // 10ms内随机等待后重试
 	}
 }
 
@@ -272,13 +558,20 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.me = me
 
 	// Your initialization code here (2A, 2B, 2C).
+	// 初始化rf值
+	// 这部分尚没有goroutine并发访问，不需要加锁
+	rf.currentTerm = 0
+	rf.setFollower()
+	rf.flushElectionTimer()
+	rf.log = append(rf.log, LogEntry{Command: "init", Term: rf.currentTerm})
+	rf.commitIndex = 0
+	rf.lastApplied = 0
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
 
 	// start ticker goroutine to start elections
 	go rf.ticker()
-
 
 	return rf
 }
